6d1ed5c5f1048257f3530bbbe83af43f5c0e2496
Dani <ognenoski777dani@gmail.com>
2026-05-27 18:31:08 +0200

    domashna

---
+++ js-4/script.js
@@ -0,0 +1,28 @@
+async function getClub() {
+
+    try {
+
+        const response = await fetch(
+            "https://www.thesportsdb.com/api/v1/json/3/searchteams.php?t=Milan"
+        )
+
+        const data = await response.json()
+
+        const club = data.teams[0]
+
+        document.getElementById("club-name").textContent = club.strTeam
+        document.getElementById("stadium").textContent = club.strStadium
+        document.getElementById("founded").textContent = club.intFormedYear
+        document.getElementById("club-city").textContent = club.strLocation
+        document.getElementById("league").textContent = club.strLeague
+        document.getElementById("logo").src = club.strBadge
+
+    } catch (error) {
+
+        console.log(error)
+
+    }
+
+}
+
+getClub()
